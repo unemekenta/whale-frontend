adefaeb403d8dd88113813ad1b75b9e9be0f0ff7
unemekenta <unemekenta@gmail.com>
2023-07-09 23:22:44 +0900

    Fix: デザイン修正

--- pages/diary/detail/_id.vue
+++ pages/diary/detail/_id.vue
@@ -5,10 +5,37 @@
         <h1>{{ diary.title }}</h1>
       </v-col>
     </v-row>
-    <v-divider class="my-3"></v-divider>
-    <p class="my-3 text-right">
-      <small>{{ fmtDateWithoutTime(diary.date) }}</small>
-    </p>
+    <v-divider justify="justify-space-between" class="my-3"></v-divider>
+    <v-row class="align-center my-1">
+      <v-col cols="10">
+        <p class="ma-0">
+          <small>{{ fmtDateWithoutTime(diary.date) }}</small>
+        </p>
+      </v-col>
+      <v-col cols="2">
+        <v-row justify="end">
+          <v-avatar size="40" class="avatar">
+            <v-img
+              v-if="diary.user.image"
+              :src="diary.user.image"
+              :aspect-ratio="1"
+              alt="avatarImage"
+              class="avatar-image"
+            />
+            <v-img
+              v-else
+              :src="require('@/assets/images/common/icon-user.png')"
+              :aspect-ratio="1"
+              alt="avatarImage"
+              class="avatar-image"
+            />
+          </v-avatar>
+          <p class="text-center my-auto nickname">
+            <small>{{ diary.user.nickname }}</small>
+          </p>
+        </v-row>
+      </v-col>
+    </v-row>
     <p class="text-body-2">{{ diary.content }}</p>
     <v-row>
       <v-col v-for="image in diary.images" :key="image.id" cols="4">
@@ -34,6 +61,12 @@ interface Image {
   caption: string
 }
 
+interface User {
+  id: number
+  image: string
+  nickname: string
+}
+
 interface Diary {
   id: number
   title: string
@@ -41,6 +74,7 @@ interface Diary {
   public: boolean
   date: string
   images: Image[]
+  user: User
 }
 
 @Component({
@@ -60,6 +94,11 @@ export default class DiaryDetail extends Vue {
     public: false,
     date: "",
     images: [],
+    user: {
+      id: 0,
+      nickname: "",
+      image: "",
+    },
   }
 
   fmtDateWithoutTime(date: string) {
@@ -77,5 +116,10 @@ export default class DiaryDetail extends Vue {
 <style>
 .text-body-2 {
   white-space: pre-wrap;
+}
+
+.nickname {
+  display: inline;
+  vertical-align: center;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -13,10 +13,44 @@
               :src="require('@/assets/images/common/reading-glasses.png')"
               :aspect-ratio="16 / 9"
             ></v-img>
-            <v-card-title
-              ><p class="line-clamp-1">{{ diary.title }}</p></v-card-title
-            >
-            <v-card-subtitle>{{ fmtDateWithoutTime(diary.date) }}</v-card-subtitle>
+            <v-card-title class="my-1 py-1">
+              <p class="line-clamp-1 my-0">
+                {{ diary.title }}
+              </p>
+            </v-card-title>
+            <v-row justify="justify-space-between" class="my-0 mx-2 align-center">
+              <v-col cols="7" class="my-0 py-0">
+                <v-row>
+                  <v-avatar size="30" class="avatar">
+                    <v-img
+                      v-if="diary.user.image"
+                      :src="diary.user.image"
+                      :aspect-ratio="1"
+                      alt="avatarImage"
+                      class="avatar-image"
+                    />
+                    <v-img
+                      v-else
+                      :src="require('@/assets/images/common/icon-user.png')"
+                      :aspect-ratio="1"
+                      alt="avatarImage"
+                      class="avatar-image"
+                    />
+                  </v-avatar>
+                  <p class="text-center my-auto nickname">
+                    <small>{{ diary.user.nickname }}</small>
+                  </p>
+                </v-row>
+              </v-col>
+              <v-col cols="5" class="my-0 py-0">
+                <v-card-subtitle class="ma-0 pa-0">
+                  <p class="text-right ma-0 py-0">
+                    {{ fmtDateWithoutTime(diary.date) }}
+                  </p>
+                </v-card-subtitle>
+              </v-col>
+            </v-row>
+
             <v-card-text
               ><p class="line-clamp-2">{{ diary.content }}</p></v-card-text
             >
@@ -31,12 +65,19 @@
 import { Component, Vue } from "nuxt-property-decorator"
 import { dateWithoutTimeFilter } from "@/plugins/filter/date-filter"
 
+interface User {
+  id: number
+  nickname: string
+  image: string
+}
+
 interface Diary {
   id: number
   title: string
   content: string
   public: boolean
   date: string
+  user: User
 }
 
 @Component({
